a30fb89f2486321259e1f2a6e737a59b1039abc1
eudouabunda <44900662+eudouabunda@users.noreply.github.com>
2022-03-07 15:48:29 -0300

    Delete APIS directory

--- APIS/user1.js
+++
@@ -1,2 +0,0 @@
-fetch("https://tokyo.tokyocity.repl.co/user1/386215881966878721").then((res) => res.json()).then((json) => { Object.keys(json).forEach((key) => { const elem = document.getElementsByClassName(key); elem[0].textContent = json[key]; }); }) 
-
--- APIS/user2.js
+++
@@ -1,1 +0,0 @@
-fetch("https://tokyo.tokyocity.repl.co/user2/853352916072661012").then((res) => res.json()).then((json) => { Object.keys(json).forEach((key) => { const elem = document.getElementsByClassName(key); elem[0].textContent = json[key]; }); }) 
